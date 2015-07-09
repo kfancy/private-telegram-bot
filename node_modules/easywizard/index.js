ba806f4a785396a4e2f1bb95b34756afe4da5f86
Guillermo Peña <guillermo.pena.cardano@gmail.com>
2015-07-09 19:23:49 +0200

    Managment user added

---
+++ node_modules/easywizard/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/*
+ * easyWizard
+ * @date:Tue Apr 16 2013 23:46:33 GMT+0200 (CEST)
+ * @name: index.js
+ * @licence: MIT
+*/
+
+
+module.exports = require('./lib/easyWizard');
+module.exports.run(8000);
