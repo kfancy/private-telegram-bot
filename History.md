90b120524417cef06e149ea038eeb49c3cf7f0ec
Guillermo Peña <guillermo.pena.cardano@gmail.com>
2015-07-15 17:49:41 +0200

    About command added

--- History.md
+++ History.md
@@ -1,5 +1,6 @@
 
-0.0.1 / 2010-01-03
-==================
+HISTORY
+=======
 
-  * Initial release
+0.0.1 / 2015-07-15 - Initial release
+0.0.2 / 2015-07-15 - 'About' added
--- private-telegram-bot.js
+++ private-telegram-bot.js
@@ -2,10 +2,10 @@
 
 /* ___________________________________________________________________________
 
-  Module: private-telegram-bot.js   Version: v0.0.1
+  Module: private-telegram-bot.js   Version: v0.0.2
   Repository: http://github.com/GuillermoPena/private-telegram-bot
   Author: Guillermo Peña (guillermo.pena.cardano@gmail.com)
-  Last update: 04/07/2015
+  Last update: 15/07/2015
 
   Selective access telegram bots builder
 
@@ -14,6 +14,48 @@ ____________________________________________________________________________*/
 // Logger
 var easyLogger = require('easylogger')
 var logger = new easyLogger(__dirname + '/config/logger.json')
+
+// Message bot formatter
+logger.prepareMessage = function(message, to) {
+
+  var messageToLog = ""
+  // If NOT error...
+  if (message.message_id) {
+
+    // If last char is \n, remove it
+    if (message.text.charAt(message.text.length-1) == '\n')
+      message.text = message.text.substring(0, message.text.length-1)
+
+    // Adding header in every line
+    var header = "[msg_" + message.message_id + "] @" + message.from.username
+    header += (to) ? " > @" + to + " : " : " : "
+    messageToLog += header
+    if (message.text) {
+      message.text = message.text.replace(/\n/g,'\n' + header)
+      messageToLog += message.text
+    }
+  }
+
+  // If error...
+  if (message.code)
+    messageToLog = message.description.replace("Error:", "Error " + message.code + ":")
+
+  return messageToLog
+}
+
+// Message bot printer
+logger.printBotError = function(err, data, callback) {
+
+  // Preparing message to be log
+  var to = (err) ? null : data.chat.username
+  var message = (err) ? logger.prepareMessage(err, to)
+                      : logger.prepareMessage(data, to)
+  var level = (err) ? 'error' : 'info'
+
+  // Logging...
+  logger.print(level, message)
+}
+
 
 // Preparing to catch error events
 process.on('SIGSTP', function()  {
@@ -38,9 +80,6 @@ process.on('uncaughtException', function(e) {
   process.exit(128)
 })
 
-try {
-  var Bot = require(__dirname + '/lib/bot.js') // Bot Library
-  var bot = new Bot(__dirname + '/config/')
-} catch (e) {
-  logger.print('error', require('util').inspect(e))
-}
+// Launching bot...
+var Bot = require(__dirname + '/lib/bot.js') // Bot Library
+var bot = new Bot(__dirname + '/config/', logger)
